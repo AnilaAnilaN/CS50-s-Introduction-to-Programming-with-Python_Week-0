8447444ac480926fdd8c2bbba8baf9cfbc318af7
Anila Nawaz <anilanawaz531@gmail.com>
2026-01-04 11:58:48 +0500

    "Solution: my solution for the playback problem"

---
+++ playback/playback.py
@@ -0,0 +1,11 @@
+def main():
+
+    #take input from the user and store it in a variable
+    userInput = input("Please, write your thoughts here: ")
+
+    #modify the user input by replacing every space with ... and print the result
+    print(userInput.replace(" ", "..."))
+
+
+
+main()
